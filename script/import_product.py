8adc0010057e92c6a03f098e7559e1df0e672ebc
nicothebrush <nicola.riolini@gmail.com>
2020-04-06 09:04:16 +0200

    generic bugfix (fast message)

--- script/import_product.py
+++ script/import_product.py
@@ -40,7 +40,7 @@ server = config.get('dbaccess', 'server')
 port = config.get('dbaccess', 'port')   # verify if it's necessary: getint
 
 file_in = './data/artioerp.txt'
-force_start = 85313
+force_start = False
 
 # -----------------------------------------------------------------------------
 # Utility:
