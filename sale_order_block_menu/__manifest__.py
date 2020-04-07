d7412c298d602256b99cf4fc55c9216a9d7508d6
nicothebrush <nicola.riolini@gmail.com>
2020-04-07 23:28:53 +0200

    [ADD] sale_order_block_menu: start master menu with permission, add management groups

--- sale_order_block_menu/__manifest__.py
+++ sale_order_block_menu/__manifest__.py
@@ -2,27 +2,24 @@
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
 
 {
-    'name': 'Sale order in block',
+    'name': 'Minimal sale block menu',
     'version': '11.0.2.0.0',
     'category': 'Sales',
     'description': """
         Manage report block for sales
         """,
-    'summary': 'Sale, Block, report',
+    'summary': 'Sale, Block, menu',
     'author': 'Micronaet S.r.l. - Nicola Riolini',
     'website': 'http://www.micronaet.it',
     'license': 'AGPL-3',
     'depends': [
         'sale',
         'sale_management',
+        'sale_order_block',
         'web',
     ],
     'data': [
-        'security/sales_group.xml',
-        'security/ir.model.access.csv',
-        
-        'views/block_view.xml',
-        'report/sales_report.xml',
+        'views/menu_view.xml',
     ],
     'external_dependencies': {},
     'application': False,
